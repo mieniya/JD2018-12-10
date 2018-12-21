edc473f02570abbe6bc4b596916d7483170ac6de
Skwazer <Skwazer@mail.ru>
2018-12-21 12:18:04 +0300

    Level 6 Tasks A Completed

---
+++ src/by/it/agadzhanov/jd01_06/TaskA1.java
@@ -0,0 +1,21 @@
+package by.it.agadzhanov.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA1 {
+    public static void main(String[] args) {
+        String reg="[а-яА-ЯёЁ]{4,}";
+        Pattern pattern=Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(Poem.text);
+        StringBuilder sb=new StringBuilder(Poem.text);
+
+        while (matcher.find()) {
+            int start=matcher.start();
+            sb.setCharAt(start+3,'#');
+            if (matcher.end()-matcher.start()>=7)
+                sb.setCharAt(start+6,'#');
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/by/it/agadzhanov/jd01_06/TaskB1.java
@@ -0,0 +1,7 @@
+package by.it.agadzhanov.jd01_06;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+
+    }
+}
