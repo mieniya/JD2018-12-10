c94cb15c5b48b8be00224152f060b794ec342a7a
AleksandrAgadzhanov <alexagadzhanov97@gmail.com>
2018-12-23 00:35:11 +0300

    Lecture 6 Level B Complete

--- src/by/it/agadzhanov/jd01_06/TaskC1.java
+++ src/by/it/agadzhanov/jd01_06/TaskC1.java
@@ -1,7 +1,16 @@
 package by.it.agadzhanov.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskC1 {
     public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder(Poem.text);
+        Pattern newline=Pattern.compile("\n");
+        Matcher matcherNewline=newline.matcher(sb);
 
+        while (matcherNewline.find()) {
+            System.out.println(matcherNewline.group());
+        }
     }
 }
