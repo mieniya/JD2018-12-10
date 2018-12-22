14646b1e18e99a6956bf58d87186ba9fdc887071
AleksandrAgadzhanov <alexagadzhanov97@gmail.com>
2018-12-22 13:13:25 +0300

    Lecture 6 Tasks Intermediate Push

---
+++ src/by/it/agadzhanov/jd01_06/TaskB1.java
@@ -0,0 +1,28 @@
+package by.it.agadzhanov.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            if (wordCheck(matcher.group())==true)
+                System.out.println(matcher.group());
+        }
+    }
+
+    private static boolean vowelCheck(char a) {
+        if (a=='а' || a=='о' || a=='у' || a=='э' || a=='ы' || a=='я' || a=='ё' || a=='ю' || a=='е' || a=='и')
+            return true;
+        else return false;
+    }
+
+    private static boolean wordCheck(String word) {
+        if (vowelCheck(word.charAt(word.length()-1))==true && vowelCheck(word.charAt(0))==false) {
+            return true;
+        }
+        else return false;
+    }
+}
