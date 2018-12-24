7ad2a27fda36a633855fe726ef5b7cf560303135
demologin <demo@it-academy.by>
2018-12-24 12:34:47 +0300

    jd01_07 A Classwork

---
+++ src/by/it/_tasks_/jd01_07/Vector.java
@@ -0,0 +1,24 @@
+package by.it._tasks_.jd01_07;
+
+
+public class Vector extends Var{
+
+    private double[] value;
+
+    Vector(double[] value) {
+        this.value=new double[value.length];
+        System.arraycopy(value,0,this.value,0,value.length);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out=new StringBuilder("{");
+        String delimiter="";
+        for (double v : value) {
+            out.append(delimiter).append(v);
+            delimiter=", ";
+        }
+        out.append('}');
+        return out.toString();
+    }
+}
