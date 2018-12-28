e7c194e85884039510c607bffa747f7faad114a5
Gizmik <GizmikIT@gmail.com>
2018-12-28 12:24:11 +0300

    Level 8 start

---
+++ src/by/it/yarmolenka/jd01_08/Var.java
@@ -0,0 +1,30 @@
+package by.it.yarmolenka.jd01_08;
+
+abstract class Var implements Operation {
+
+
+    @Override
+    public Var add(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Переменная";
+    }
+}
