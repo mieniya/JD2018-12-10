a2ecb1a9b0423b45b967f110a431c5dd38dcc65b
Андрей <andrey23081989@mail.ru>
2018-12-28 15:29:54 +0400

    Home work lesson 8

--- src/by/it/yarmolenka/jd01_08/Var.java
+++ src/by/it/yarmolenka/jd01_08/Var.java
@@ -5,21 +5,25 @@ abstract class Var implements Operation {
 
     @Override
     public Var add(Var other) {
+        System.out.println("Сложение невозможно");
         return null;
     }
 
     @Override
     public Var sub(Var other) {
+        System.out.println("Вычитание невозможно");
         return null;
     }
 
     @Override
     public Var mul(Var other) {
+        System.out.println("Умножение невозможно");
         return null;
     }
 
     @Override
     public Var div(Var other) {
+        System.out.println("Деление невозможно");
         return null;
     }
 
