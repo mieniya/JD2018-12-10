19051a4cd000d376ab8c6ce788c6a2a0a1d5729e
anaron <naronartem@gmail.com>
2019-01-04 16:58:16 +0300

    jd01_08 work on oopShip

--- src/by/it/naron/jd01_08/oopShip/Ship.java
+++ src/by/it/naron/jd01_08/oopShip/Ship.java
@@ -1,5 +1,5 @@
 package by.it.naron.jd01_08.oopShip;
 
 public interface Ship {
-    void swim ();
+    String swim ();
 }
--- src/by/it/naron/jd01_08/oopShip/WarShip.java
+++ src/by/it/naron/jd01_08/oopShip/WarShip.java
@@ -1,13 +1,13 @@
 package by.it.naron.jd01_08.oopShip;
 
  abstract class WarShip implements Ship {
-    void shoot (){
-        System.out.println("I can shoot");
+     int distance;
+
+    void shoot (int distance){
+        this.distance=distance;
+        System.out.println("I can shoot - "+distance+" distance");
     }
-    @Override
-    public void swim (){
-        System.out.println("i can swim");
-    }
+
 
      @Override
      public String toString() {
