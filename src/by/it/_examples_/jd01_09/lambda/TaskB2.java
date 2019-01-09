67a93d2fad6cd141ad7f78ff8d2dba52ee9fea78
demologin <demo@it-academy.by>
2019-01-09 12:22:12 +0300

    jd01_09 A Classwork

--- src/by/it/_examples_/jd01_09/lambda/TaskB2.java
+++ src/by/it/_examples_/jd01_09/lambda/TaskB2.java
@@ -8,22 +8,24 @@ class TaskB2 {
     public static void main(String[] args) {
         String[] sentences = Poem.text.split("[.!?]\n");
         for (int i = 0; i < sentences.length; i++)
-            sentences[i]=sentences[i].replaceAll("[^А-Яа-яёЁ]+", " ");
+            sentences[i] = sentences[i].replaceAll("[^А-Яа-яёЁ]+", " ");
 
-        Comparator<String> comparator1=new Comparator<String>() {
+        Comparator<String> comparator1 = new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return o1.length()-o2.length();
+                return o1.length() - o2.length();
             }
         };
-        Comparator<String> comparator2= (o1, o2) -> o1.length()-o2.length();
+//        Comparator<String> comparator2 = (o1, o2) -> o1.length() - o2.length();
+//
+//        Arrays.sort(sentences, comparator1);                             //способ 1
+//
+//        Arrays.sort(sentences, comparator2);                             //способ 2
+//        Arrays.sort(sentences, (o1, o2) -> o1.length() - o2.length());    //способ 3
+        Arrays.sort(sentences, (s1, s2) -> s2.length() - s1.length());
 
-        Arrays.sort(sentences,comparator1);                             //способ 1
 
-        Arrays.sort(sentences,comparator2);                             //способ 2
-        Arrays.sort(sentences, (o1, o2) -> o1.length()-o2.length());    //способ 3
-
-        Arrays.sort(sentences, Comparator.comparingInt(String::length));//способ 4
+        //Arrays.sort(sentences, Comparator.comparingInt(String::length));//способ 4
 
         for (String sentence : sentences) {
             System.out.println(sentence);
