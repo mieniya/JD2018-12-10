8daeae4bb5ebab2dfaafd140e49de6f8f1834b29
anaron <naronartem@gmail.com>
2019-01-12 14:14:17 +0300

    jd01_10

--- src/by/it/naron/jd01_10/PrintMath.java
+++ src/by/it/naron/jd01_10/PrintMath.java
@@ -6,17 +6,19 @@ import java.lang.reflect.Modifier;
 public class PrintMath {
     public static void main(String[] args) {
         Class<Math> mathClass = Math.class;
-        Method[] methods = mathClass.getDeclaredMethods();
-        for (Method method : methods){
+        Method[] methodsArr = mathClass.getDeclaredMethods();
+        StringBuilder mm = new StringBuilder();
+
+        for (Method method : methodsArr){
             String name = method.getName();
             int modifiers = method.getModifiers();
-            StringBuilder mm = new StringBuilder();
             if (Modifier.isPublic(modifiers)) mm.append("public");
             if (Modifier.isPrivate(modifiers)) mm.append("private");
             if (Modifier.isProtected(modifiers)) mm.append("protected");
             if (Modifier.isStatic(modifiers)) mm.append("static");
             Class<?> returnType = method.getReturnType();
             String ret = returnType.getSimpleName();
+
             System.out.println("");
         }
     }
