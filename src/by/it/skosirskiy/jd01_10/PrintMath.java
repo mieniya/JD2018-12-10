18e07430c89ef37940b530d22e89f27a92fa63ea
skosirskiy <x987110@gmail.com>
2019-01-14 08:38:29 +0300

    1

--- src/by/it/skosirskiy/jd01_10/PrintMath.java
+++ src/by/it/skosirskiy/jd01_10/PrintMath.java
@@ -1,7 +1,25 @@
 package by.it.skosirskiy.jd01_10;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class PrintMath {
     public static void main(String[] args) {
+        Class<Math> ss = Math.class;
+        Method[] methods = ss.getDeclaredMethods();
+        for (Method method : methods) {
+            String name = method.getName();
+            int modifiers = method.getModifiers();
+            StringBuilder mm = new StringBuilder();
+            if (Modifier.isPublic(modifiers)) mm.append("public ");
+            if (Modifier.isPrivate(modifiers)) mm.append("private ");
+            if (Modifier.isProtected(modifiers)) mm.append("protected ");
+            if (Modifier.isStatic(modifiers)) mm.append("static ");
+            Class<?> returnType = method.getReturnType();
+            String ret = returnType.getSimpleName();
+            if (Modifier.isStatic(modifiers))
+                System.out.printf("%s%s %s\n", mm, ret, name);
+        }
 
 
 
