bc7443a9c193e59d1118d9e95e2c17f00fd6b3e4
anaron <naronartem@gmail.com>
2019-01-14 12:17:44 +0300

    jd01_11 started.

---
+++ src/by/it/naron/jd01_11/Runner.java
@@ -0,0 +1,28 @@
+package by.it.naron.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("1");
+        a.add("2");
+        a.add("3");
+        a.add("4");
+        a.add("5");
+
+        List<String> b = new ArrayList<>();
+        a.add("1");
+        a.add("2");
+        a.add("3");
+        a.add("4");
+        a.add("5");
+
+
+
+
+
+
+        }
+}
