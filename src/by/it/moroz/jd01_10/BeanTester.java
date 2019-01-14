b195800a6880c46471bd82179f24db6703fcd162
AlenaMoroz <black.panther.92.hu@gmail.com>
2019-01-14 07:44:42 +0300

    jd01_10 completed

--- src/by/it/moroz/jd01_10/BeanTester.java
+++ src/by/it/moroz/jd01_10/BeanTester.java
@@ -1,15 +1,22 @@
 package by.it.moroz.jd01_10;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 
 public class BeanTester {
 
     public static void main(String[] args) throws Exception {
 
-        Class bean = Bean.class;
-        Annotation[] annotations = bean.getAnnotations();
-        
+        Class<Bean> bean = Bean.class;
+        Method[] declaredMethods = bean.getDeclaredMethods();
+
+        for (Method declaredMethod : declaredMethods) {
+            if (declaredMethod.isAnnotationPresent(Param.class)){
+                System.out.println(declaredMethod.getName()+ " " +
+                        declaredMethod.invoke(bean.getDeclaredConstructor().newInstance(), declaredMethod.getAnnotation(Param.class).a(), declaredMethod.getAnnotation(Param.class).b()));
+            }
+        }
+
     }
 
 }
