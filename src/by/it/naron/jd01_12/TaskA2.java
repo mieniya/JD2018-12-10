7bf7c705091ebae913e9982d6b4436c093479aec
temauser <naronartem@gmail.com>
2019-01-16 12:24:34 +0300

    jd01_12

---
+++ src/by/it/naron/jd01_12/TaskA2.java
@@ -0,0 +1,13 @@
+package by.it.naron.jd01_12;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        Integer [] arr1 = {0,1,1,1,1,2,2,2,3,3,4,4,5,5,6,6,7,7,100};
+        Integer [] arr2 = {9,9,9,8,8,8,7,7,7,6,6,6,5,4,4,4};
+
+
+
+
+
+    }
+}
