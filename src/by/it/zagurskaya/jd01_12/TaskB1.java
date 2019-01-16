20b34ee1a5b27e3cb6363f616d626a171c894922
Zagurskaya Natalia <zagurskaya.nl86@gmail.com>
2019-01-17 01:10:30 +0300

    jd01_12_home

--- src/by/it/zagurskaya/jd01_12/TaskB1.java
+++ src/by/it/zagurskaya/jd01_12/TaskB1.java
@@ -1,31 +1,35 @@
 package by.it.zagurskaya.jd01_12;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class TaskB1 {
     public static void main(String[] args) {
-
         String expression;
         String regAllWord = " ";
         Scanner scanner = new Scanner(System.in);
 
+        List<String> wordList = new ArrayList<>();
         while (!(expression = scanner.nextLine()).equals("end")) {
-            expression = expression.replaceAll("[\\n,:.-]", " ");
-            expression = expression.replaceAll("  ", " ");
-            String[] expressionMass = expression.split(regAllWord);
-
-//            for (String s : expressionMass) {
-//                System.out.printf(s+" ");
-//            }
-            HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
-            for (int i = 0; i < expression.length(); i++) {
-                System.out.println(expressionMass[i]);
-//                Map<String, Integer> varMap = Integer.getVars();
-//                for (wordCount.Entry<String, Integer> varEntry : wordCount.entrySet()) {
-////                System.out.print(varEntry.getKey() + "=");
-//                System.out.println((varEntry.getValue()));
+            expression = expression.replaceAll("[\\n,:;.]", " ");
+            expression = expression.replaceAll("\\s[\\s]+", " ");
+            wordList.addAll(Arrays.asList(expression.split(regAllWord)));
+        }
+        
+        Map<String, Integer> varMap = new HashMap<>();
+        for (String wordString : wordList) {
+            if (varMap.containsKey(wordString)) {
+                Integer count = varMap.get(wordString) + 1;
+                varMap.put(wordString, count);
+            } else {
+                varMap.put(wordString, 1);
             }
+        }
+        mapPrint(varMap);
+    }
+
+    public static void mapPrint(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.println(e.getKey() + "=" + e.getValue());
         }
     }
 }
