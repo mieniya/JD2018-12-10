0901f10c447b71d9a0138b5d960983e1001df3df
VladimirKasiyanov <vladimirqw@gmail.com>
2019-01-16 12:30:10 +0300

    task 12

---
+++ src/by/it/migunko/jd01_12/TaskA1.java
@@ -0,0 +1,10 @@
+package by.it.migunko.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TaskA1 {
+
+
+}
---
+++ src/by/it/migunko/jd01_12/TaskA2.java
@@ -0,0 +1,14 @@
+package by.it.migunko.jd01_12;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        Integer[] arr1={0,8,1,1,1,2,3,3,3,4,4};
+        Integer[] arr2={9,9,8,8,7,7,6};
+        Set<Integer> set1= new HashSet<>(Arrays.asList(arr1));
+
+    }
+}
