fff2c305ecac9bc2a982a033835713774bfb8192
skosirskiy <x987110@gmail.com>
2019-01-17 15:39:27 +0300

    1

--- src/by/it/skosirskiy/jd01_12/TaskB1.java
+++ src/by/it/skosirskiy/jd01_12/TaskB1.java
@@ -3,25 +3,40 @@ package by.it.skosirskiy.jd01_12;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.Pattern;
+
 
 public class TaskB1 {
     private static Map<String, Integer> hashmap=new HashMap<String, Integer>();
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
+
         String str;
 
 
+        System.out.println("do while");
+
+
+
         while((!(str= scanner.next()).equals("end"))){
-           if(hashmap.containsKey(str)){
-               hashmap.put(str,hashmap.get(str)+1);
+
+
+            String strbuf = str.replaceAll("[*,-.—:;]", "");
+            if (!(strbuf.isEmpty())) {
+
+
+           if(hashmap.containsKey(strbuf)){
+               hashmap.put(strbuf,hashmap.get(strbuf)+1);
 
 
            }
-           else hashmap.put(str, 1);
-        }
+           else hashmap.put(strbuf, 1);
+         }
+    }
         System.out.println(hashmap);
+        }
+
+
 
     }
 
-}
+
