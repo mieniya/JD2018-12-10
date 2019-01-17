5a052cd0b482403d9260cb5503368941822eb495
migunko <evgeniya.migunko@gmail.com>
2019-01-17 21:08:37 +0200

    Task 12

--- src/by/it/migunko/jd01_12/TaskA1.java
+++ src/by/it/migunko/jd01_12/TaskA1.java
@@ -6,5 +6,26 @@ import java.util.List;
 
 public class TaskA1 {
 
+    private List<Integer> grades=new ArrayList<>();
 
+    public static void main(String[] args) {
+        TaskA1 instance=new TaskA1();
+        for (int i = 0; i < 10; i++) {
+            int grade=(int)(Math.random()*10)+1;
+            instance.grades.add(grade);
+        }
+        System.out.println(instance.grades);
+        instance.clearBad(instance.grades);
+        System.out.println(instance.grades);
+    }
+
+    private void clearBad(List<Integer> gr) {
+        Iterator<Integer> iterator = gr.iterator();
+        while (iterator.hasNext()){
+            Integer current=iterator.next();
+            if (current<4)
+                iterator.remove();
+        }
+
+    }
 }
--- src/by/it/migunko/jd01_12/TaskA2.java
+++ src/by/it/migunko/jd01_12/TaskA2.java
@@ -3,12 +3,33 @@ package by.it.migunko.jd01_12;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class TaskA2 {
     public static void main(String[] args) {
-        Integer[] arr1={0,8,1,1,1,2,3,3,3,4,4};
-        Integer[] arr2={9,9,8,8,7,7,6};
-        Set<Integer> set1= new HashSet<>(Arrays.asList(arr1));
+        Integer[] arr1 = {0, 8, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7};
+        Integer[] arr2 = {9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 5, 4, 4, 4};
 
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(arr1));
+        Set<Integer> set2 = new TreeSet<>(Arrays.asList(arr2));
+        System.out.println(set1);
+        System.out.println(set2);
+
+        Set<Integer> union = getUnion(set1, set2);
+        System.out.println(union);
+        Set<Integer> cross = getCross(set1, set2);
+        System.out.println(cross);
+    }
+
+    private static Set<Integer> getUnion(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set1);
+        result.addAll(set2);
+        return result;
+    }
+
+    private static Set<Integer> getCross(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set1);
+        result.retainAll(set2);
+        return result;
     }
 }
