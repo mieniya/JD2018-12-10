9bec368187b11122f2617f524dda6ad6eec59b4d
skosirskiy <x987110@gmail.com>
2019-01-17 16:48:06 +0300

    1

--- src/by/it/skosirskiy/jd01_12/TaskB1.java
+++ src/by/it/skosirskiy/jd01_12/TaskB1.java
@@ -9,34 +9,21 @@ public class TaskB1 {
     private static Map<String, Integer> hashmap=new HashMap<String, Integer>();
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-
         String str;
-
-
         System.out.println("do while");
-
-
 
         while((!(str= scanner.next()).equals("end"))){
 
-
-            String strbuf = str.replaceAll("[*,-.—:;]", "");
-            if (!(strbuf.isEmpty())) {
-
-
+           String strbuf = str.replaceAll("[*,-.—:;]", "");
+           if (!(strbuf.isEmpty())) {
            if(hashmap.containsKey(strbuf)){
                hashmap.put(strbuf,hashmap.get(strbuf)+1);
-
-
            }
            else hashmap.put(strbuf, 1);
          }
     }
         System.out.println(hashmap);
         }
-
-
-
     }
 
 
---
+++ src/by/it/skosirskiy/jd01_12/TaskB2.java
@@ -0,0 +1,31 @@
+package by.it.skosirskiy.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TaskB2 {
+    private static String process(ArrayList<String> peoples){
+        return "";
+    }
+    static String process1(LinkedList<String> peoples){
+        Iterator<String> iterator = peoples.iterator();
+        
+return "";
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("Barsik");
+        linkedList.add("Murzik");
+        linkedList.add("Ryzhik");
+        linkedList.add("rizik");
+        linkedList.add("seriyk");
+        linkedList.add("cherniy");
+        linkedList.add("beliy");
+        linkedList.add("ciniy");
+        process1(linkedList);
+    }
+}
