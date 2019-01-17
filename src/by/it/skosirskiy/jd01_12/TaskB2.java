370bda4145f9ddfe6a70e5d2d0b2ecd44336773b
skosirskiy <x987110@gmail.com>
2019-01-18 00:04:32 +0300

    1

--- src/by/it/skosirskiy/jd01_12/TaskB2.java
+++ src/by/it/skosirskiy/jd01_12/TaskB2.java
@@ -3,29 +3,64 @@ package by.it.skosirskiy.jd01_12;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
 public class TaskB2 {
     private static String process(ArrayList<String> peoples){
         return "";
     }
-    static String process1(LinkedList<String> peoples){
+    static int count=0;
+    static String process(LinkedList<String> peoples){
         Iterator<String> iterator = peoples.iterator();
-        
-return "";
+
+        while (iterator.hasNext()){
+            if(count==0) iterator.next();
+            if (!iterator.hasNext()){
+                System.out.println("first break");
+                break;
+
+            }
+            iterator.next();
+            if(!iterator.hasNext()) {
+                System.out.println("second break");
+                break;}
+            iterator.remove();
+            count=0;
+            while (iterator.hasNext()){
+                iterator.next();
+                count=1;
+                if (iterator.hasNext())
+                {
+                    iterator.next();
+                    iterator.remove();
+                    count=0;
+                }
+            }
+
+            process(peoples);
+
+
+        }
+
+
+        return peoples.element();
     }
 
     public static void main(String[] args) {
 
         LinkedList<String> linkedList = new LinkedList<>();
-        linkedList.add("Barsik");
-        linkedList.add("Murzik");
-        linkedList.add("Ryzhik");
-        linkedList.add("rizik");
-        linkedList.add("seriyk");
-        linkedList.add("cherniy");
-        linkedList.add("beliy");
-        linkedList.add("ciniy");
-        process1(linkedList);
+        linkedList.add("111");
+        linkedList.add("222");
+        linkedList.add("333");
+        linkedList.add("444");
+        linkedList.add("555");
+        linkedList.add("666");
+        linkedList.add("777");
+        linkedList.add("888");
+
+
+
+
+        System.out.println(process(linkedList));
+
     }
 }
