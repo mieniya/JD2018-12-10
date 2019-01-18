94b20e48c37ad9087eb0e091fb2f5ea0f953b27e
anaron <naronartem@gmail.com>
2019-01-18 12:18:47 +0300

    jd01_13 start

---
+++ src/by/it/naron/jd01_13/TaskA.java
@@ -0,0 +1,22 @@
+package by.it.naron.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+        try {
+            if (Math.random()>0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        }catch (NullPointerException e){
+
+
+        }
+
+
+
+
+    }
+}
