b89549d05b93f179913edc9b8e36adde90dea6da
anaron <naronartem@gmail.com>
2019-01-20 01:05:06 +0300

    jd01_13 a,b added

--- src/by/it/naron/jd01_13/TaskA.java
+++ src/by/it/naron/jd01_13/TaskA.java
@@ -10,12 +10,31 @@ public class TaskA {
                 new HashMap<String, String>(null);
             else
                 Integer.parseInt("привет");
-        }catch (NullPointerException e){
+        }catch (Exception e){
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement element: stackTrace){
+                if (TaskA.class.getName().equals(element.getClassName())){
+                    System.out.println(element);
+                    String name = e.getClass().getName();
+                    String clName = element.getClassName();
+                    int number = element.getLineNumber();
+                    System.out.printf(
+                            "name: %s\n" +
+                                    "class: %s\n"+
+                                    "line: %d", name,clName,number);
+                    break;
+                }
+
+
+            }
 
 
         }
 
 
+
+        //  NullPointerException
+        //NumberFormatException
 
 
     }
