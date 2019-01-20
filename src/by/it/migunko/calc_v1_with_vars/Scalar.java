3e22d37d45a57912321d5e65587e91d19e79c316
migunko <evgeniya.migunko@gmail.com>
2019-01-20 21:56:56 +0200

    calc

---
+++ src/by/it/migunko/calc_v1_with_vars/Scalar.java
@@ -0,0 +1,66 @@
+package by.it.migunko.calc_v1_with_vars;
+
+public class Scalar extends Var {
+    private double value;
+
+    public double getValue() {
+        return value;
+    }
+
+    Scalar(double value) {
+        this.value = value;
+    }
+
+    Scalar(Scalar otherScalar) {
+        this.value=otherScalar.value;
+    }
+
+    Scalar(String strValue) {
+        value=Double.parseDouble(strValue);
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double res=this.value+((Scalar) other).value;
+            return new Scalar(res);
+        }
+        return other.add(this);
+        //other Vector or Matrix
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double res=this.value-((Scalar) other).value;
+            return new Scalar(res);
+        }
+        //other Vector or Matrix
+        Scalar minus=new Scalar(-1);
+        return other.add(this.mul(minus));
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double res=this.value*((Scalar) other).value;
+            return new Scalar(res);
+        }
+        //other Vector or Matrix
+        return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double res=this.value/((Scalar) other).value;
+            return new Scalar(res);
+        }
+        return super.div(other);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
