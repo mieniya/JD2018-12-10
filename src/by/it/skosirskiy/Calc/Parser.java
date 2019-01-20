856219d24f13f5c203ee0b2d229014d19120f16e
skosirskiy <x987110@gmail.com>
2019-01-20 14:59:49 +0300

    1

---
+++ src/by/it/skosirskiy/Calc/Parser.java
@@ -0,0 +1,34 @@
+package by.it.skosirskiy.Calc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    Var calc(String expression){
+        String [] operand = expression.split(Patterns.OPERATION);
+        Var two= Var.createVar(operand[1]);
+        if (expression.contains("=")){
+           return Var.saveVar(operand[0],two);
+        }
+        Var one= Var.createVar(operand[0]);
+
+        if (one == null || two == null)
+            return null; //TODO create error
+        Pattern p = Pattern.compile(Patterns.OPERATION);
+        Matcher m = p.matcher(expression);
+        if (m.find()){
+
+            String operation = m.group();
+            switch (operation){
+                case "+":return one.add(two);
+                case "-":return  one.sub(two);
+                case "*":return  one.mul(two);
+                case "/":return  one.div(two);
+            }
+
+        }
+        return null;
+    }
+
+}
