9fbf3e3888a7214c4e056f78a0dfb541d0ae3db5
anaron <naronartem@gmail.com>
2019-01-21 11:52:09 +0300

    jd01_14 start

---
+++ src/by/it/naron/jd01_14/TaskA.java
@@ -0,0 +1,21 @@
+package by.it.naron.jd01_14;
+
+public class TaskA {
+
+    static String getPath(Class<?> clazz, String fileName){
+        clazz.getName();
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+
+      //показывает путь
+     //   System.out.println(System.getProperty("user.dir"));
+        String path = "D:\\DeV\\it-academy\\javaCourse_Khmelev\\" +
+                "MyCourseWithGit\\src\\by\\it\\naron\\jd01_14";
+    }
+}
