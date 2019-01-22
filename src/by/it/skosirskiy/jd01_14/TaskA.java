3bb3d3974fc8cdc10b33be37d0e62cd91b3a3b34
skosirskiy <x987110@gmail.com>
2019-01-22 16:09:47 +0300

    1

--- src/by/it/skosirskiy/jd01_14/TaskA.java
+++ src/by/it/skosirskiy/jd01_14/TaskA.java
@@ -14,70 +14,52 @@ TaskA.java.
  Вывести с новой строки их среднее арифметическое avg=20.123.
  Продублировать вывод в консоль в файл resultTaskA.txt*/
 public class TaskA {
-
-    private static String src = System.getProperty("user.dir")+"\\src\\by\\it\\skosirskiy\\";
-    private static String filename=src+"jd01_14/dataTaskA.bin";
-    private static String filename1=src+"jd01_14/resultTaskA.txt";
-    static File f = new File(filename);
-    static File f1 = new File(filename1);
-    static Collection<Integer> col = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        getRandomMas();
-
-
-        try (DataInputStream inp = new DataInputStream(new BufferedInputStream
-                (new FileInputStream(filename)))) {
-            double sum=0;
-            double count=0;
-            Integer value=0;
-            while (inp.available()>0){
-                value=(inp.readInt());
-                col.add(value);
-                count++;
-                sum=sum+value;
-                System.out.print(value+" ");
-            }
-            System.out.println("avg="+ sum/count);
-
-        }
-        catch (IOException e){
-            System.out.println("FilenotFound: "+filename);
-        }
-
-        DataOutputStream dos =null;
-        try {
-            dos = new DataOutputStream(new FileOutputStream(f1));
-            for (Integer element : col) {
-                dos.writeInt(element);
-            }
-
-        }
-        catch (IOException e){
-            System.out.println("FilenotFound: "+filename);
-        }
-        finally {
-            if(dos!=null){dos.close();}
-        }
-
-
+    private static String dir (Class<?> cl){
+        String path=System.getProperty("user.dir")+File.separator+"src"+File.separator;
+        String clDir=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
+        return path+clDir;
 
     }
-
-
-
-    private static void getRandomMas() throws IOException {
-        DataOutputStream dos =null;
+    public static void main(String[] args) {
+        System.out.println(dir(TaskA.class));
+        DataOutputStream dos=null;
         try {
-           dos = new DataOutputStream(new FileOutputStream(f));
+            dos=new DataOutputStream(new BufferedOutputStream(
+                    new FileOutputStream(dir(TaskA.class)+"dataTaskA.bin")));
             for (int i = 0; i < 20; i++) {
-                dos.writeInt((int)(Math.random()*3000));
+                dos.writeInt((int)(Math.random()*25));
+            }
+        }
+        catch (IOException e){}
+        finally {
+            if(dos !=null){
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            }
+        }
+        try (DataInputStream inp=new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(dir(TaskA.class)+"dataTaskA.bin")));
+                PrintWriter out2= new PrintWriter(new FileWriter(dir(TaskA.class)+"resultTaskA.txt"))
+        ){
+                double sum=0;
+                double count=0;
+                while (inp.available()>0){
+                    int value=(inp.readInt());
+                    System.out.print(value+" ");
+                    out2.print(value+" ");
+                    count++;
+                    sum=sum+value;
+                }
+            System.out.println("\navg="+sum/count);
+                out2.print("\navg="+sum/count);
+
         }
         catch (IOException e){
-            System.out.println("FilenotFound: "+filename);
-        }
-        finally {
-            if(dos!=null){dos.close();}
+            e.printStackTrace();
         }
     }
 
