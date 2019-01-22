7bb5af25c830a81739b0e2808f7da94859d3d623
subach <peregaro@gmail.com>
2019-01-23 01:55:35 +0300

    jd01_14 all done

--- src/by/it/subach/jd01_14/TaskB.java
+++ src/by/it/subach/jd01_14/TaskB.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB {
+
     static String getPath(Class<?> clazz, String filename) {
         String path = clazz.getName();
         String simpleName = clazz.getSimpleName();
