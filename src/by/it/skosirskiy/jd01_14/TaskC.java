8384a2862e21a008bf1a7883d6ef3582dc13b8ec
skosirskiy <x987110@gmail.com>
2019-01-22 17:07:12 +0300

    1

---
+++ src/by/it/skosirskiy/jd01_14/TaskC.java
@@ -0,0 +1,12 @@
+package by.it.skosirskiy.jd01_14;
+/* Вывести список всех файлов и каталогов вашего пакета by.it.фамилия в
+формате file:имя_файла или dir:имя_каталога.
+ Продублировать вывод в консоль в файл resultTaskC.txt*/
+public class TaskC {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
