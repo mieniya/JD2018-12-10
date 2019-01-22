1b4dc63a573468aff6bfcdf21f81d8ed8d06f064
WildFunky <peregaro@gmail.com>
2019-01-22 21:10:07 +0300

    Level 01_14 TaskC start

--- src/by/it/subach/jd01_14/TaskA.java
+++ src/by/it/subach/jd01_14/TaskA.java
@@ -12,7 +12,6 @@ public class TaskA {
         path = path.replace(simpleName, "");
         path = path.replace(".", File.separator);
         path = System.getProperty("user.dir") + File.separator + "src" + File.separator + path;
-        System.out.println(path);
         return path+filename;
     }
 
@@ -21,20 +20,11 @@ public class TaskA {
         String path = getPath(TaskA.class, "dataTaskA.bin");
         System.out.println(path);
 
-        try(
-            FileOutputStream fos = new FileOutputStream(path);
-            DataOutputStream dos = new DataOutputStream(fos)
-        )
-        {
-            for (int i = 0; i < 20; i++) {
-                dos.writeInt((int)(Math.random()*50));
-            }
 
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        writeBinaryFileWithInt(path);
 
         List<Integer> list = new ArrayList<>();
+
         double sum = 0;
         try {
             DataInputStream dis = new DataInputStream(new FileInputStream(path));
@@ -47,14 +37,9 @@ public class TaskA {
             e.printStackTrace();
         }
 
-//        print to console
-        for (Integer integer : list) {
-            System.out.print(integer + " ");
-        }
-        System.out.println();
 
-        System.out.println(list);
-        System.out.println("avg = " + sum/list.size());
+             //print to console
+
 
 //        print to file
 
@@ -64,10 +49,38 @@ public class TaskA {
             for (Integer integer : list) {
                 pw.print(integer + " ");
             }
+            pw.println("\navg=" + sum /list.size());
+            pw.close();
         }catch (IOException e){
             e.printStackTrace();
         }
+        printToConsole(list, sum);
 
+    }
+
+    private static void writeBinaryFileWithInt(String path) {
+        try(
+                FileOutputStream fos = new FileOutputStream(path);
+                DataOutputStream dos = new DataOutputStream(fos)
+        )
+        {
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int)(Math.random()*50));
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void printToConsole(List<Integer> list, double sum) {
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+
+        System.out.println(list);
+        System.out.println("avg=" + sum/list.size());
     }
 
 }
