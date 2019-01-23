ae97103b9ca223a8efac65042cdb5c6f4a23ba70
subach <peregaro@gmail.com>
2019-01-23 18:05:19 +0300

    jd01_15 start

---
+++ src/by/it/subach/jd01_09/CalcV3/Operation.java
@@ -0,0 +1,8 @@
+package by.it.subach.jd01_09.CalcV3;
+
+interface Operation {
+    Var add(Var other) throws CalcException;
+    Var sub(Var other) throws CalcException;
+    Var mul(Var other) throws CalcException;
+    Var div(Var other) throws CalcException;
+}
