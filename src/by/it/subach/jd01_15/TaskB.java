0b2af920283035c571f2184a4454cd2a7e011227
subach <peregaro@gmail.com>
2019-01-25 02:07:00 +0300

    jd01_15 complete

--- src/by/it/subach/jd01_15/TaskB.java
+++ src/by/it/subach/jd01_15/TaskB.java
@@ -1,61 +1,56 @@
 package by.it.subach.jd01_15;
-
-
 import java.io.*;
+/*
+this
+comment
+must
+be delete
+ */
 
 public class TaskB {
-    /**
-     * This program delete all JavaDocs,
-     * oneLine comments,
-     * and manyLine comments...
-     * Or not delete =)
-     *
-     * @param
-     */
 
     static String getPath(Class<?> clazz, String filename) {
         String path = clazz.getName();
-        String simpleName = clazz.getSimpleName();         //worked too
+        String simpleName = clazz.getSimpleName();
         path = path.replace(simpleName, "");
         path = path.replace(".", File.separator);
         path = System.getProperty("user.dir") + File.separator + "src" + File.separator + path;
         return path + filename;
     }
 
-/*
-some comment 1
-
- */
 
     public static void main(String[] args) throws IOException {
 
         String path = getPath(TaskB.class, "TaskB.java");
         System.out.println(path);
         StringBuilder sb = new StringBuilder();
-
-/*
-
-some comment 2
+/**
+ *some
+ *Java
+ *Doc
+ *
+ *
  */
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
             int c;
             while ((c = reader.read()) != -1) {
-                if (c == 47) {
+                if (c == 47) {                  //working too
                     if (reader.read() == 47) {
-                            while (reader.read() != 13) {
-                                reader.read();
+                       reader.readLine();
+                       sb.append("\n");
+                    }
+                    else {
+                        c = reader.read();
+                        while (c != 47) {
+                            c = reader.read();
                         }
-                    } else
-                        while (reader.read() != 47) {
-                            reader.read();
-                        }
+                    }
                 }
                 if (c != 47)
                     sb.append((char) c);
             }
-//            some comment 3
-
+//comment
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -63,11 +58,13 @@ some comment 2
         BufferedWriter writer = new BufferedWriter(new FileWriter(getPath(TaskB.class, "TaskB.txt")));
         writer.write(sb.toString());
         writer.close();
-//          some comment 4
+//comment
         System.out.println(sb);
     }
 }
 
+
+// and here too
 
 
 
