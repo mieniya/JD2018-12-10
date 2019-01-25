c1fecc48474cb1fc0fc562c4d1c9cb27cb830268
RohauAlexandr@gmail.com <Al5356160>
2019-01-25 12:28:40 +0300

    jd02_01

---
+++ src/by/it/zagurskaya/jd02_01/Buyer.java
@@ -0,0 +1,43 @@
+package by.it.zagurskaya.jd02_01;
+
+//public class Buyer extends Thread implements IBuyer {
+//    public Buyer() {
+//        super("Buyer #№"+number);
+//    }
+//
+//    public Buyer(Runnable target) {
+//        super(target);
+//    }
+//
+//    public Buyer(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    public void run() {
+//        enterToMarket();
+//        chooseGoods();
+//        goOut();
+//        System.out.flush();
+//    }
+//
+//    @Override
+//    public void enterToMarket() {
+//        System.out.println(this+"enter to Market");
+//
+//    }
+//
+//    @Override
+//    public void chooseGoods() {
+//        Util.getRandom(500,200);
+//        Util.sleep(timeout);
+//        System.out.println(this+"chous Market");
+//
+//    }
+//
+//    @Override
+//    public void goOut() {
+//        System.out.println(this+"goo out from Market");
+//
+//    }
+//}
---
+++ src/by/it/zagurskaya/jd02_01/Runner.java
@@ -0,0 +1,31 @@
+package by.it.zagurskaya.jd02_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+//    static List<Bayer> byuers = ArrayList<>();
+//    public static void main(String[] args) {
+//
+//        int number = 0;
+//        for (int time = 1; i < 100; time++) {
+//            int count = Util.getRandom(2);
+//            for (int i = 0; i < count; i++) {
+//                Buyer buyer = new Buyer(++number);
+//                byuers.add(buyer);
+//                Dispatcher.countBauer++;
+//                buyer.start();
+//            }
+//            Util.sleep(1000);
+//        }
+//        for (Buyer buyer : byuers) {
+//            try {
+//                buyer.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println("Market close");
+//    }
+
+}
