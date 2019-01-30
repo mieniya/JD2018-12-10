4bf654a55305ecb0a776c944797d889b4edf983b
AlenaMoroz <black.panther.92.hu@gmail.com>
2019-01-30 11:14:37 +0300

    jd02_03

---
+++ src/by/it/moroz/jd02_03/Cashier.java
@@ -0,0 +1,48 @@
+package by.it.moroz.jd02_03;
+
+class Cashier implements Runnable {
+
+    private String name;
+
+    Cashier(int number) {
+        name = "Cashier №" + number;
+    }
+
+    @Override
+    public void run() {
+        synchronized (Util.PRINTER) {
+            System.out.println(this + " opened");
+        }
+        while (!Util.planComplete()) {
+            Buyer pensioner = DequeBuyers.poll(DequeBuyers.pensionerDeque);
+            if (pensioner != null) {
+                serveTheBuyer(pensioner);
+            } else {
+                Buyer buyer = DequeBuyers.poll(DequeBuyers.buyerDeque);
+                if (buyer != null)
+                    serveTheBuyer(buyer);
+                else Util.sleep(1);
+            }
+        }
+        synchronized (Util.PRINTER) {
+            System.out.println(this + " closed");
+        }
+    }
+
+    private void serveTheBuyer(Buyer buyer) {
+        int time = Util.getRandom(2000, 5000);
+        Util.sleep(time);
+        synchronized (Util.PRINTER) {
+            System.out.println(this + " service " + buyer);
+            buyer.takeCheck();
+        }
+        synchronized (buyer.getMonitor()) {
+            buyer.notify();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/by/it/moroz/jd02_03/Market.java
@@ -0,0 +1,49 @@
+package by.it.moroz.jd02_03;
+
+public class Market {
+
+    public static void main(String[] args) {
+
+        System.out.println("Market opened");
+        GoodsInMarket.checkTheGoods(GoodsInMarket.goods);
+        int numBuyer = 0;
+        int numCashier = 1;
+        for (int i = 1; i <= 2; i++) {
+            Thread cashier = new Thread(new Cashier(numCashier++));
+            Buyer.threads.add(cashier);
+            cashier.start();
+        }
+        while (Util.marketOpened()) {
+            for (int sec = 1; sec <= 60; sec++) {
+                if (sec < 30) {
+                    if (Util.getCounterBuyerInMarket() <= sec + 10)
+                        getNewBuyer(++numBuyer);
+                } else {
+                    if (Util.getCounterBuyerInMarket() <= 40 + (30 - sec))
+                        getNewBuyer(++numBuyer);
+                }
+                Util.sleep(1000);
+            }
+        }
+        for (Thread thread : Buyer.threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Market closed");
+    }
+
+    private static void getNewBuyer(int numBuyer) {
+        int count = Util.getRandom(0, 2);
+        for (int j = 0; j < count; j++)
+            if (Util.marketOpened()) {
+                Buyer buyer = new Buyer(++numBuyer);
+                if (numBuyer % 4 == 0)
+                    buyer.setPensioner(true);
+                else buyer.setPensioner(false);
+                buyer.start();
+            }
+    }
+}
