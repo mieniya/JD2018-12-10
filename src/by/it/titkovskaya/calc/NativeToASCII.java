9516dfb784096f0697ce70c5429a967196ca4b8f
Airina <i.titkovskaya@gmail.com>
2019-02-07 15:17:43 +0300

    jd02_05 complete

--- src/by/it/titkovskaya/calc/NativeToASCII.java
+++ src/by/it/titkovskaya/calc/NativeToASCII.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class NativeToASCII {
     public static void main(String[] args) {
-        File dir = new File(System.getProperty("user.dir")  + "/src/by/it/titkovskaya/calc");
+        File dir = new File(System.getProperty("user.dir")  + "/src/by/it/titkovskaya/calc/internationalization");
         if (dir.exists() && dir.isDirectory()){
             File[] files = dir.listFiles();
             if (files != null) {
