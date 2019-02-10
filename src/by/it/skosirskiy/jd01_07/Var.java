8bb8c498c096dfea3b2403a3d65473edfa9eadcf
skosirskiy <x987110@gmail.com>
2019-02-10 12:17:49 +0300

    all working

--- src/by/it/skosirskiy/jd01_07/Var.java
+++
@@ -1,9 +0,0 @@
-package by.it.skosirskiy.jd01_07;
-
-abstract class Var {
-
-    @Override
-    public String toString() {
-        return "Это класс Var";
-    }
-}
--- src/by/it/skosirskiy/jd02_01/IUseBacket.java
+++
@@ -1,6 +0,0 @@
-package by.it.skosirskiy.jd02_01;
-
-public interface IUseBacket {
-    void takeBacket(); //взял корзину
-    void putGoodsToBacket(); //положил выбранный товар в корзину
-}
