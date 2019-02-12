4b64114edcbcf052736f497a91c70a10df1020cf
Khmelov <akhmelev@gmail.com>
2019-02-12 23:55:55 +0300

    sss

---
+++ src/by/it/skosirskiy/calculator/ResMan.java
@@ -0,0 +1,35 @@
+package by.it.skosirskiy.calculator;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@SuppressWarnings("FieldCanBeLocal")
+public enum ResMan {
+
+    INSTANCE;
+
+    private final String RESOURSE="by/it/skosirskiy/calculator/messages";
+    private Locale locale;
+    private ResourceBundle resourceBundle;
+
+    ResMan() {
+        setLocale(Locale.getDefault());
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+        resourceBundle=ResourceBundle.getBundle(RESOURSE,locale);
+    }
+
+    public void setLocale(String language) {
+        setLocale(new Locale(language));
+    }
+
+    public void setLocale(String language, String country) {
+        setLocale(new Locale(language,country));
+    }
+
+    public String get(String key){
+        return resourceBundle.getString(key);
+    }
+}
