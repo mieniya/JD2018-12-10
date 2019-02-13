14821f4788ac57cf714c07b9535ad46d53c68e66
migunko <evgeniya.migunko@gmail.com>
2019-02-13 22:56:35 +0200

    jd02_08

--- src/by/it/migunko/jd02_08/SAXParser.java
+++ src/by/it/migunko/jd02_08/SAXParser.java
@@ -6,5 +6,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 public class SAXParser {
-
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        String xmlFile=System.getProperty("user.dir")+
+                "/src/by/it/migunko/jd02_07/travels.xml";
+        SAXHandler myHandler=new SAXHandler();
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        javax.xml.parsers.SAXParser saxParser = saxParserFactory.newSAXParser();
+        saxParser.parse(xmlFile,myHandler);
+    }
 }
