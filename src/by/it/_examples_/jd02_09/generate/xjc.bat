dc57f4887a8e2628e2559cafc81aa2270f2bd221
yarmolenka <andrey23081989@mail.ru>
2019-02-17 14:48:49 +0300

    refactor jd02_08

--- src/by/it/_examples_/jd02_09/generate/xjc.bat
+++ src/by/it/_examples_/jd02_09/generate/xjc.bat
@@ -6,7 +6,7 @@
 @echo ну и конечно, на linux будет совершенно другой синтаксис
 
 set path_xjc="C:\Program Files\Java\jdk1.8.0_111\bin\xjc.exe"
-set path_to_src="D:\it-academy\java\my\JD2018-12-10\src"
+set path_to_src="D:\students\akhmelev\JD2017-02-20\src"
 set file_xcd="D:\students\akhmelev\JD2017-02-20\src\by\it\_examples_\jd02_09\generate\xml_schema.xsd"
 
 :: Пути могут быть и абсолютными, вроде таких
