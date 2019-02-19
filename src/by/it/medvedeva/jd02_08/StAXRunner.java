ef797f7e8c605ea1af5de83cbe64430997f5b911
Annaela <annascantech@gmail.com>
2019-02-20 01:29:44 +0300

    03_01 c

--- src/by/it/medvedeva/jd02_08/StAXRunner.java
+++ src/by/it/medvedeva/jd02_08/StAXRunner.java
@@ -1,70 +1,73 @@
 package by.it.medvedeva.jd02_08;
 
+
+
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 public class StAXRunner {
 
-    private static String xml = System.getProperty("user.dir") + "/src/by/it/medvedeva/jd02_07/person+xsd.xml";
 
     public static void main(String[] args) {
-        String tab="";
-        StringBuilder content=new StringBuilder();
+        StAXRunner runner = new StAXRunner();
+        String xmlFile = System.getProperty("user.dir") +
+                "/src/by/it/medvedeva/jd02_07/person+att+xsd.xml";
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        try (Reader reader =
+                     new BufferedReader(
+                             new FileReader(xmlFile)
+                     )
+        ) {
+            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(reader);
+            StringBuilder xml = runner.process(xmlStreamReader);
+            System.out.println(xml);
 
-        try {
-            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-            FileInputStream xmlFile=new FileInputStream(xml);
-            XMLStreamReader reader = inputFactory.createXMLStreamReader(xmlFile);
-            while (reader.hasNext()){
-                int type = reader.next();
-                switch (type){
-                    case XMLStreamConstants.START_DOCUMENT:
-                        System.out.println("start");
-                        break;
-                    case XMLStreamConstants.ATTRIBUTE:
-                        System.out.println("============================================================");
-                        break;
-                    case XMLStreamConstants.START_ELEMENT:
-                        StringBuilder att = new StringBuilder();
-                        int attributeCount = reader.getAttributeCount();
-                        for (int i = 0; i < attributeCount; i++) {
-                            String name = reader.getAttributeLocalName(i);
-                            String value = reader.getAttributeValue(i);
-                            att.append(" ").append(name).append("=\"").append(value).append("\"");
-                        }
-                        System.out.println(tab + "<" + reader.getLocalName() + att + ">");
-                        tab = "\t" + tab;
-                        break;
-                    case XMLStreamConstants.CHARACTERS:
-                        String part = reader.getText();
-                        content.append(part.trim());
-                        break;
-                    case XMLStreamConstants.END_ELEMENT:
-                        if (content.length() > 0){
-                            System.out.println(tab + content);
-                            content.setLength(0);
-                        }
-
-                        tab = tab.substring(1);
-                        System.out.println(tab + "</" + reader.getLocalName() + ">");
-                        break;
-                    case XMLStreamConstants.END_DOCUMENT:
-                        System.out.println("end");
-                        break;
-                }
-            }
-
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (XMLStreamException e) {
+        } catch (IOException | XMLStreamException e) {
             e.printStackTrace();
         }
+    }
 
+    private StringBuilder process(XMLStreamReader xml) throws XMLStreamException {
+        StringBuilder xmlTxt = new StringBuilder();
+        StringBuilder elementTxt = new StringBuilder();
+        String tab = "";
+        while (xml.hasNext()) {
+            int type = xml.next();
+            switch (type) {
+                case XMLStreamConstants.START_ELEMENT:
+                    xmlTxt.append(tab).append("<").append(xml.getLocalName());
+                    int attCount = xml.getAttributeCount();
+                    for (int i = 0; i < attCount; i++) {
+                        String name = xml.getAttributeLocalName(i);
+                        String value = xml.getAttributeValue(i);
+                        String attText = String.format(" %s=\"%s\"", name, value);
+                        xmlTxt.append(attText);
+                    }
+                    tab = tab.concat("\t");
+                    xmlTxt.append(">\n");
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    String text = elementTxt.toString().trim();
+                    if (!text.isEmpty()) {
+                        xmlTxt.append(tab).append(text).append("\n");
+                        elementTxt.setLength(0);
+                    }
+                    tab = tab.substring(1);
+                    xmlTxt.append(tab).append("</").append(xml.getLocalName()).append(">\n");
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    elementTxt.append(xml.getText());
+                    break;
+            }
+
+        }
+        return xmlTxt;
     }
 }
+
