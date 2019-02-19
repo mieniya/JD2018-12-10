71602b6a1734f2b9efd8c06ed646911b4b86057f
migunko <evgeniya.migunko@gmail.com>
2019-02-19 17:34:37 +0300

    task 03_02

--- src/by/it/migunko/jd03_01/B_AddRoles.java
+++ src/by/it/migunko/jd03_01/B_AddRoles.java
@@ -4,5 +4,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 public class B_AddRoles {
+    public static void main(String[] args) {
 
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try (Connection connection =
+                     DriverManager.getConnection(
+                             "jdbc:mysql://127.0.0.1:2016/"+
+                                     "?useUnicode=true&characterEncoding=UTF-8",
+                             "root",
+                             "");
+             Statement statement = connection.createStatement()){
+
+            statement.executeUpdate("INSERT INTO `migunko`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Administrator');");
+            statement.executeUpdate("INSERT INTO `migunko`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Agent');");
+            statement.executeUpdate("INSERT INTO `migunko`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Buyer');");
+
+            if (connection.isClosed()) System.out.println("Соединение с базой прервано");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
