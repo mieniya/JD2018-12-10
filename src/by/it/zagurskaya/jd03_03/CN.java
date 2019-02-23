acc48529f11c1982b8c8010e24e10bbeb7df17ed
Zagurskaya Natalia <zagurskaya.nl86@gmail.com>
2019-02-23 20:10:39 +0300

    jd03_03

---
+++ src/by/it/zagurskaya/jd03_03/CN.java
@@ -0,0 +1,12 @@
+package by.it.zagurskaya.jd03_03;
+
+public interface CN {
+//    ??????? почему!!!!!!!!!!!!
+    String URL =
+//            "jdbc:mysql://127.0.0.1:2016/" +
+            "jdbc:mysql://127.0.0.1:2016/zagurskaya" +
+                    "?useUnicode=true&characterEncoding=UTF-8";
+    String USER = "root";
+    String PASSWORD = "";
+
+}
