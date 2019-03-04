82325649a709cddd7a22764ee136bca52c8b5b6c
skosirskiy <x987110@gmail.com>
2019-03-04 15:47:56 +0300

    project

--- src/by/it/skosirskiy/project/java/controller/CmdLogout.java
+++ src/by/it/skosirskiy/project/java/controller/CmdLogout.java
@@ -1,11 +1,15 @@
 package by.it.skosirskiy.project.java.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class CmdLogout implements Cmd {
     @Override
     public Action execute(HttpServletRequest req) {
         req.getSession().invalidate();
+        HttpServletResponse resp =
+                (HttpServletResponse) req.getAttribute("resp");
+
         return Action.LOGIN;
     }
 }
