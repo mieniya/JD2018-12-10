339e312d135255d4159d4d9a4316d00e1fa1e537
Zagurskaya <Pfuehcrfz1986>
2019-03-11 11:19:26 +0300

    project_Cookie

--- src/by/it/zagurskaya/project/java/controller/CmdLogout.java
+++ src/by/it/zagurskaya/project/java/controller/CmdLogout.java
@@ -2,15 +2,13 @@ package by.it.zagurskaya.project.java.controller;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 public class CmdLogout implements Cmd {
     @Override
     public Action execute(HttpServletRequest req) throws Exception {
         req.getSession().invalidate();
+        Cookie cookie = new Cookie("login", "none");
+        Util.setCookie(req, cookie);
         return Action.LOGIN;
     }
 }
